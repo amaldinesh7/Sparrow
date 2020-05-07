893f5c82ccbb65889fa08a5867da0902ac6fc27a
amaldinesh4549 <amalkdinesh@gmail.com>
2020-05-07 16:24:41 +0530

    privateRoute js file added

---
+++ sparrow-frontend/src/components/shared/Navigation/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      localStorage.getItem('accessToken') === 'null'
+        ? <Component {...props} />
+        : <Redirect to='/' />
+    )} />
+  );
+
+  export default PrivateRoute;
