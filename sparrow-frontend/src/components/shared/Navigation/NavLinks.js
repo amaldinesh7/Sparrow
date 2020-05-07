08e7ea1afaad9756a24b7f7a9758510688813d80
amaldinesh4549 <amalkdinesh@gmail.com>
2020-05-07 19:33:32 +0530

    :zap:login using gapi completed

--- sparrow-frontend/src/components/shared/Navigation/NavLinks.js
+++ sparrow-frontend/src/components/shared/Navigation/NavLinks.js
@@ -3,20 +3,39 @@ import { NavLink } from 'react-router-dom';
 
 import './NavLinks.css';
 
-const NavLinks = props => {
-    return (
-        <ul className="nav-links">
-            <li>
-                <NavLink to="/" exact>All Users</NavLink>
-            </li>
-            <li>
-                <NavLink to="/newuser">Add User</NavLink>
-            </li>
-            <li>
-                <NavLink to="/auth">Logout</NavLink>
-            </li>
-        </ul>
-    );
+class NavLinks extends React.Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '604711657196-dlfe9tsb18bk696qjvo27acenihcuebv.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+            })
+        })
+    }
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+        localStorage.setItem('accessToken', [null]);
+    }
+    render() {
+        return (
+            <ul className="nav-links">
+                <li>
+                    <NavLink to="/users" exact>All Users</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/newuser">Add User</NavLink>
+                </li>
+                <li>
+                    <NavLink className="logout" to="/" onClick={this.onSignOutClick}>Logout</NavLink>
+                </li>
+            </ul>
+        );
+    }
+
 };
 
 export default NavLinks;
